51840026a6b2ab565fd05d8c73bc11b1fd867922
shanedoc <shanedoc@163.com>
2020-03-17 19:38:12 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //user
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
