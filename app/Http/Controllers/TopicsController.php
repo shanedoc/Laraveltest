617618ba35aa98b40a294b694440f7378020b51e
shanedoc <shanedoc@163.com>
2020-03-19 22:15:53 +0800

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,9 +25,14 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Topic $topic,Request $request)
     {
-        return view('topics.show', compact('topic'));
+        // URL 矫正
+        if ( !empty($topic->slug) && $topic->slug != $request->slug) {
+            dd($topic->link());
+            return redirect($topic->link(), 301);
+        }
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
     }
 
 	public function create(Topic $topic)
@@ -41,7 +46,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic)
@@ -56,7 +61,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
 	}
 
 	public function destroy(Topic $topic)
--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,10 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //user
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController',
+    ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
