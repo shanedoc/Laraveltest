c558904e1a0382c61d2c70a6293b257b2b10affa
shanedoc <shanedoc@163.com>
2020-03-19 11:23:08 +0800

    创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -12,16 +12,16 @@ class TopicRequest extends Request
             case 'POST':
             {
                 return [
-                    // CREATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
-                return [
-                    // UPDATE ROLES
-                ];
+
             }
             case 'GET':
             case 'DELETE':
@@ -35,7 +35,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
