71e8008e737d0aa9c1ffcffb1af90edefff88805
shanedoc <shanedoc@163.com>
2020-03-22 20:41:03 +0800

    队列翻译

---
+++ app/Jobs/TranslateSlug.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Handlers\SlugTranslateHandler;
+use App\Models\Topic;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class TranslateSlug implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $topic;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Topic $topic)
+    {
+        $this->topic = $topic;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info('jobs in');
+        $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
+        DB::table('topic')->where('id',$this->topic->id)->update([
+            'slug'=>$slug
+        ]);
+    }
+}
