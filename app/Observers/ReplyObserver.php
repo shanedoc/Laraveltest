3e13d1b2f62fdf48a4ab19859c8bd46ebab3303e
shanedoc <shanedoc@163.com>
2020-03-23 15:52:46 +0800

    用户发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,19 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+
     public function creating(Reply $reply)
     {
-        //
+        //dd(clean($reply->content, 'user_topic_body'));
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        //$reply->topic->increment('reply_count', 1);
+        //先创建，成功后再对话题下的所有回复进行统计
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
     public function updating(Reply $reply)
